58785266cc208589bfa66038d4ea7be49afe551b
Christian Aurich <christian.aurich@c-aurich.de>
2016-02-11 17:36:58 +0100

    adding more tests to the testsuite

--- test/test_eagle-bom.py
+++ test/test_eagle-bom.py
@@ -28,3 +28,22 @@ def test_simple_brd():
         assert 0
 
     assert filecmp.cmp('/tmp/simple.csv', 'test/files/'+simple_brd_csv) 
+
+def test_cli():
+    try:
+        #call script with garbage parameters
+        retcode = call("python eagle-bom.py" + " --foobar", shell=True)
+        assert retcode == 2
+        #call script without input file
+        retcode = call("python eagle-bom.py --csv=/tmp/simple.csv", shell=True)
+        assert retcode == 3
+        #call script without output file
+        retcode = call("python eagle-bom.py" + " --brd=test/files/" + simple_brd, shell=True)
+        assert retcode == 4
+        #try to pass an invalid output type
+        retcode = call("python eagle-bom.py" + " --brd=test/files/" + simple_brd + " --csv=/tmp/simple.csv -t foobar", shell=True)
+        assert retcode == 5
+        #TODO: add more cli tests
+    except OSError as e:
+        assert 0
+
