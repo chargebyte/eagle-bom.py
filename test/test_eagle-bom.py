967df0b307107607e1650f63bccca55a9e9a281a
Christian Aurich <christian.aurich@c-aurich.de>
2016-03-12 12:15:41 +0100

    introducing tests for stdin/stdout support (#18)

--- test/test_eagle-bom.py
+++ test/test_eagle-bom.py
@@ -29,15 +29,60 @@ def test_simple_brd():
 
     assert filecmp.cmp('/tmp/simple.csv', 'test/files/'+simple_brd_csv) 
 
-def test_cli():
+def test_cli_errors():
     try:
         #call script with garbage parameters
-        retcode = call("python eagle-bom.py" + " --foobar", shell=True)
+        retcode = call("python eagle-bom.py --foobar >/tmp/stderr", shell=True)
         assert retcode == 2	
         #try to pass an invalid output type
         retcode = call("python eagle-bom.py" + " --in=test/files/" + simple_brd + " --out=/tmp/simple.csv -t foobar", shell=True)
         assert retcode == 5
-        #TODO: add more cli tests
+    except OSError as e:
+        assert 0
+
+def test_cli_stdin():
+    try:
+        #call script by passing brd via stdin
+        retcode = call("cat test/files/" + simple_brd + "|python eagle-bom.py --out=/tmp/simple.csv", shell=True)
+        assert retcode == 0
+        assert filecmp.cmp('/tmp/simple.csv', 'test/files/'+simple_brd_csv) 
+
+        #call script by passing sch via stdin
+        retcode = call("cat test/files/" + simple_sch + "|python eagle-bom.py --out=/tmp/simple.csv", shell=True)
+        assert retcode == 0
+        assert filecmp.cmp('/tmp/simple.csv', 'test/files/'+simple_sch_csv) 
+
+    except OSError as e:
+        assert 0
+
+def test_cli_stdout():
+    try:
+        #call script and return CSV via stdout
+        retcode = call("python eagle-bom.py --in=test/files/" + simple_brd + ">/tmp/simple.csv", shell=True)
+        assert retcode == 0
+        assert filecmp.cmp('/tmp/simple.csv', 'test/files/'+simple_brd_csv) 
+
+        #call script by passing sch via stdin
+        retcode = call("python eagle-bom.py --in=test/files/" + simple_sch + ">/tmp/simple.csv", shell=True)
+        assert retcode == 0
+        assert filecmp.cmp('/tmp/simple.csv', 'test/files/'+simple_sch_csv) 
+
+    except OSError as e:
+        assert 0
+
+
+def test_cli_stdin_stdout():
+    try:
+        #call script and return CSV via stdout
+        retcode = call("cat test/files/" + simple_brd + "|python eagle-bom.py>/tmp/simple.csv", shell=True)
+        assert retcode == 0
+        assert filecmp.cmp('/tmp/simple.csv', 'test/files/'+simple_brd_csv) 
+
+        #call script by passing sch via stdin
+        retcode = call("cat test/files/" + simple_sch + "|python eagle-bom.py>/tmp/simple.csv", shell=True)
+        assert retcode == 0
+        assert filecmp.cmp('/tmp/simple.csv', 'test/files/'+simple_sch_csv) 
+
     except OSError as e:
         assert 0
 
